f3d635984382807f7efb59eeeed12e51e0e84924
RafalStrab <rafal.strab@gmail.com>
2013-11-19 21:38:27 +0000

    commit

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ $( document ).ready(function() {
 		var linkurl     = $(this).attr("href");
 		var linkhtmlurl = linkurl.substring(1, linkurl.length);
 		
-		content.load(linkhtmlurl);
+		//content.load(linkhtmlurl);
 
-		//setTimeout(function() { content.load(linkhtmlurl, function() { /* no callback */ }) }, 500);
+		setTimeout(function() { content.load(linkhtmlurl, function() { /* no callback */ }) }, 500);
 	});
 });
